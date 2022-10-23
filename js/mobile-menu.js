f2255f349952bf69f20786a757a5d110f0437b6e
Liubov Kochmaruk <l.s.kochmaruk@gmail.com>
2022-10-23 23:30:26 +0200

    Added hamburger btns and include mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -6,12 +6,13 @@
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
+      
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
@@ -21,6 +22,7 @@
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
+
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
